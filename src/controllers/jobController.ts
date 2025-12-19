5dc27f699d6e54f7b0f5207f82a1a7061fe453eb
AyushJo <ayushs24101@iiitnr.edu.in>
2025-12-19 12:40:06 +0530

    Job Controllers

---
+++ src/controllers/jobController.ts
@@ -0,0 +1,128 @@
+import { Request, Response } from "express";
+import { prisma } from "../db/prisma.js";
+
+export const createJob = async (req: any, res: Response) => {
+  try {
+    const user = req.user;
+    const {
+      type,
+      templateId,
+      input,
+      priority,
+      scheduledFor,
+      parentJobId,
+      workflowId,
+    } = req.body;
+    const job = await prisma.job.create({
+      data: {
+        type,
+        templateId,
+        input,
+        priority,
+        scheduledFor,
+        parentJobId,
+        workflowId,
+        userId: user.id,
+      },
+    });
+    if (!job) {
+      return res.status(400).json({ message: "Job not created" });
+    }
+    res.status(200).json({ job, message: "Job created successfully" });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getJobs = async (req: any, res: Response) => {
+  try {
+    const user = req.user;
+    const jobs = await prisma.job.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+    res.status(200).json({ jobs });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const cancelJob = async (req: Request, res: Response) => {
+  try {
+    const { jobId } = req.params;
+    const job = await prisma.job.findFirst({
+      where: {
+        id: jobId,
+      },
+    });
+    if (!job) {
+      return res.status(400).json({ message: "Job not found" });
+    }
+    await prisma.job.delete({
+      where: {
+        id: jobId,
+      },
+    });
+    res.status(200).json({ message: "Job cancelled successfully" });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getJobById = async (req: Request, res: Response) => {
+  try {
+    const { jobId } = req.params;
+    const job = await prisma.job.findFirst({
+      where: {
+        id: jobId,
+      },
+    });
+    if (!job) {
+      return res.status(400).json({ message: "Job not found" });
+    }
+    res.status(200).json({ job });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const editJob = async (req: Request, res: Response) => {
+  try {
+    const { jobId } = req.params;
+    const {
+      type,
+      templateId,
+      input,
+      priority,
+      scheduledFor,
+      parentJobId,
+      workflowId,
+    } = req.body;
+    const job = await prisma.job.update({
+      where: {
+        id: jobId,
+      },
+      data: {
+        type,
+        templateId,
+        input,
+        priority,
+        scheduledFor,
+        parentJobId,
+        workflowId,
+      },
+    });
+    if (!job) {
+      return res.status(400).json({ message: "Job not found" });
+    }
+    res.status(200).json({ job, message: "Job updated successfully" });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ src/middlewares/extractUserDetails.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../db/prisma.js";
+export const extractUserDetails = async (req: any, res: any, next: any) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as any;
+    if (!decodedToken || !decodedToken.id) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+    const user = await prisma.user.findFirst({
+      where: {
+        id: decodedToken.id,
+      },
+    });
+    req.user = user;
+    next();
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
